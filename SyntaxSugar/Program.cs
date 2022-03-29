35db607a824286333201df37f7df1bd4642c4e27
Andrea Schwenk <andreaschwenk3@gmail.com>
2022-03-28 19:44:32 -0500

    Completing assignment

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + "is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "is greater than or equal to nine";
+            //}
+
+
+            var response =(answer <9)? ($"{answer} is less than nine") : ($"{answer} is greater than nine");
+            Console.WriteLine(response);
+        }
+    }
+}
